ffb1933eb98d1f7c5dc7951e0b8f4f67d4a025ed
Noah <scott.613865@swl.k12.oh.us>
2023-10-31 09:45:07 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double userTemp = 0;
+        boolean done = true;
+
+        do{
+            System.out.println("Enter the tempature in Celsius: ");
+            userTemp = scan.nextDouble();
+                    if(scan.hasNextDouble()){
+                        double fTemp = (userTemp * (9 / 5)) + 32;
+                    }else{
+                        System.out.println("Your input was invalid. Try Again");
+                    }
+            System.out.println("Enter 'N' to STOP.");
+                    if(userTemp = "N")
+        }while(done);
+
     }
 }
