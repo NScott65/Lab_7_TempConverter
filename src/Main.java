cef7b9ea518666e7fcb401ce5c16cdd5692ff67e
NScott65 <144457750+NScott65@users.noreply.github.com>
2023-11-01 15:05:00 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        double userTemp = 0;
-        boolean done = true;
+        double c;
+        boolean done = false;
+        double f;
+        String n = " ";
 
         do{
-            System.out.println("Enter the tempature in Celsius: ");
-            userTemp = scan.nextDouble();
-                    if(scan.hasNextDouble()){
-                        double fTemp = (userTemp * (9 / 5)) + 32;
-                    }else{
-                        System.out.println("Your input was invalid. Try Again");
+            System.out.println("Enter temperature in Celsius. Enter 'N' to STOP: ");
+                if (scan.hasNextDouble()) {
+                    c = scan.nextDouble();
+                    f = (c * 1.8) + 32;
+                    System.out.println("The temperature in fahrenheit is: " + f);
+                } else if (scan.hasNextLine()) {
+                    n = scan.nextLine();
+                    if (n.equalsIgnoreCase("N")) {
+                        done = true;
+                    } else {
+                        System.out.println("Your input was invalid. Try Again.");
                     }
-            System.out.println("Enter 'N' to STOP.");
-                    if(userTemp = "N")
-        }while(done);
+                } else {
+                    System.out.println("Your input was invalid. Try Again.");
+                }
+            
+        }while(!done);
 
     }
 }
